c6241b88ae7e83683d744649e6bfd0460b1b1bd6
Jonathan Cavalcante <jonathancmpc@gmail.com>
2020-08-04 15:30:12 -0300

    Tratando erro na insercao de dados no BD

--- src/routes.ts
+++ src/routes.ts
@@ -16,41 +16,49 @@ routes.post('/classes', async (request, response) => {
 
   const trx = await db.transaction();
 
-  /* Inserindo os dados no banco de dados, o insert sempre retorna o id dos itens que foram incluídos */
-  const insertedUsersIds = await trx('users').insert({
-    name, 
-    avatar, 
-    whatsapp, 
-    bio
-  });
+  try {
+    /* Inserindo os dados no banco de dados, o insert sempre retorna o id dos itens que foram incluídos */
+    const insertedUsersIds = await trx('users').insert({
+      name, 
+      avatar, 
+      whatsapp, 
+      bio
+    });
 
-    /* Pegando o primeiro id retornado pelo insert realizado na tabela de usuários(lembrando que é o primeiro id realizado a cada requisição e não o primeiro id da tabela) */
-  const user_id = insertedUsersIds[0];
+      /* Pegando o primeiro id retornado pelo insert realizado na tabela de usuários(lembrando que é o primeiro id realizado a cada requisição e não o primeiro id da tabela) */
+    const user_id = insertedUsersIds[0];
 
-  const insertedClassesIds = await trx('classes').insert({
-    subject,
-    cost,
-    user_id
-  });
+    const insertedClassesIds = await trx('classes').insert({
+      subject,
+      cost,
+      user_id
+    });
 
-  const class_id = insertedClassesIds[0];
+    const class_id = insertedClassesIds[0];
 
-  /* Convertendo a hora em minutos antes de inserir no banco, para isso foi criada uma função em utils */
-  const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
-    return {
-      week_day: scheduleItem.week_day,
-      from: convertHourToMinutes(scheduleItem.from),
-      to: convertHourToMinutes(scheduleItem.to),
-      class_id,
-    };
-  });
+    /* Convertendo a hora em minutos antes de inserir no banco, para isso foi criada uma função em utils */
+    const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
+      return {
+        week_day: scheduleItem.week_day,
+        from: convertHourToMinutes(scheduleItem.from),
+        to: convertHourToMinutes(scheduleItem.to),
+        class_id,
+      };
+    });
 
-  /* Inserimos então no banco de dados com os valores formatados, e passamos a variável classSchedule pq já está no formato que o banco espera */
-  await trx('class_schedule').insert(classSchedule);
+    /* Inserimos então no banco de dados com os valores formatados, e passamos a variável classSchedule pq já está no formato que o banco espera */
+    await trx('class_schedule').insert(classSchedule);
 
-  await trx.commit();
+    await trx.commit();
 
-  return response.send();
+    return response.status(201).send();
+  } catch (err) {
+    await trx.rollback(); // Desfazendo a inserção no banco
+
+    return response.status(400).json({
+      error: 'Unexpected error while creating new class'
+    });
+  }
 });
 
 export default routes;
